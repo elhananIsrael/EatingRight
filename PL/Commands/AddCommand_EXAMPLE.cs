cfd1f2f05b514fa36c540a5424566d2e1a821bed
Heller <eellhhaannaann@gmail.com>
2018-10-10 16:22:42 +0300

    הוספת דוגמאות

---
+++ PL/Commands/AddCommand_EXAMPLE.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL.Commands
+{
+    class AddCommand_EXAMPLE
+    {
+    }
+}
+
+
+/*
+ 
+ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestMVVM.Commands
+{
+    public class AddCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+           return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AddCommand(object CurrentVM)
+        {
+
+        }
+    }
+}
+
+     
+     */
---
+++ PL/Commands/SearchCommand_EXAMPLE.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PL.Commands
+{
+    class SearchCommand_EXAMPLE
+    {
+    }
+}
+
+/*
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace TestMVVM.Commands
+{
+    public class SearchCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        private ISearch CurrentVM;
+        public SearchCommand(ISearch CurrentVM)
+        {
+            this.CurrentVM = CurrentVM;
+        }
+        public bool CanExecute(object parameter)
+        {
+            bool result = false;
+
+            if (!(parameter is null))
+            {
+                var p = parameter.ToString();
+                if (p.Length > 2) result = true;
+            }
+            return result;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (!(parameter is null))
+            {
+                var p = parameter.ToString();
+                CurrentVM.SearchStudents(p);
+            }
+        }
+    }
+}
+*/
